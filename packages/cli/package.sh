d5760046181f9569f98b8cbd276823f0e21f4ec2
Kevin Stenerson <kevin@stenerson.me>
2026-01-26 00:19:59 -0500

    fix: failing test deps

--- packages/cli/package.sh
+++ packages/cli/package.sh
@@ -5,6 +5,6 @@
 # Main v0-common.sh and supporting utilities for bin/ commands.
 
 PKG_NAME="cli"
-PKG_DEPS=(core state mergeq merge worker feature hooks status)
+PKG_DEPS=(core state mergeq merge worker hooks status)
 PKG_EXPORTS=(lib/v0-common.sh)
 PKG_TEST_ONLY=false
